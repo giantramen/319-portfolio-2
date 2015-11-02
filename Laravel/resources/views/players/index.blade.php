46620c43907715776158d1b0d8f50314bf44f5a7
Jason Huntrods <jasonhuntrods@gmail.com>
2015-11-02 11:32:27 -0600

    table

--- Laravel/resources/views/players/index.blade.php
+++ Laravel/resources/views/players/index.blade.php
@@ -4,21 +4,53 @@
 		.table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
     </style>
 	
-@extends('app')
+@extends('layout')
  
 @section('content')
-    <h2>Players</h2>
-    @if ( !$players->count() )
-        You have no players availible
-    @else
-		You have ${{$moneyAvailible}} left
-		
-        <table cellpadding="5" cellspacing="0" class="db-table">
-		<tr><th>Player</th><th>Cost    </th><th>League Points</th></tr>
-		@foreach($players as $player)
-				<tr><th>{{$player->name}}</th><th>${{$player->cost}}</th><th>{{$player->leaguePoints}}</th><th><input id="addPlayer" type="button" value="Add Player"/><br /> <br/> </tr>
-		@endforeach
-		</table><br />
-		 
+		<div class="container-fluid">
+			<h2>Players</h2>
+	    	@if ( !$players->count() )
+	        	You have no players availible
+	    	@else
+				You have ${{$moneyAvailible}} left
+			<div class="row" style="margin-top: 30px;">
+	        <div class="col-md-1"></div>
+	        <div class="col-md-5">
+	        	<table cellpadding="5" cellspacing="0" class="table table-striped table-bordered table-hover">
+					<tr>
+						<th>Player</th>
+						<th>Cost</th>
+						<th>League Points</th>
+					</tr>
+					@foreach($players as $player)
+							<tr>
+								<th>{{$player->name}}</th>
+								<th>${{$player->cost}}</th>
+								<th>{{$player->leaguePoints}}</th>
+								<th><input id="addPlayer" type="button" value="Add Player"/></th>
+							</tr>
+					@endforeach
+				</table>
+			</div>
+			<div class="col-md-5">
+	        	<table cellpadding="5" cellspacing="0" class="table table-striped table-bordered table-hover">
+					<tr>
+						<th>Player</th>
+						<th>Cost</th>
+						<th>League Points</th>
+					</tr>
+					@foreach($players as $player)
+							<tr>
+								<th>{{$player->name}}</th>
+								<th>${{$player->cost}}</th>
+								<th>{{$player->leaguePoints}}</th>
+								<th><input id="addPlayer" type="button" value="Add Player"/></th>
+							</tr>
+					@endforeach
+				</table>
+			</div>
+			<div class="col-md-1"></div>
+			</div>
+		</div>
     @endif
 @endsection
